16ce532af66a1375d2eddd8edce79ca7f04904c4
geqtten <geqtten@users.noreply.github.com>
2025-11-30 18:28:14 +0000

    deploy: 679391b3785f9e70612c274d105d39a5f26e9403

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3126919608"
+    serviceWorkerVersion: "2585061991"
   }
 });
